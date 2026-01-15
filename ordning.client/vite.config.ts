98d79b0781fa6deaa6238ac448337017d1a3e7a7
Adam Tovatt <sporedenstore@hotmail.com>
2026-01-15 11:43:19 +0100

    don't use base path in release and vite config

--- ordning.client/vite.config.ts
+++ ordning.client/vite.config.ts
@@ -39,7 +39,6 @@ const target = env.ASPNETCORE_HTTPS_PORT ? `https://127.0.0.1:${env.ASPNETCORE_H
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    base: env.VITE_BASE ?? '/',
     plugins: [plugin()],
     resolve: {
         alias: {
